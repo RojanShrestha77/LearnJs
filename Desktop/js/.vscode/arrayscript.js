85bdbd864a72f4ac920ffeccd74c38bfe2dd47a5
RojanShrestha77 <shresrozan555@gmail.com>
2025-10-31 15:23:15 +0545

    rojan

--- Desktop/js/.vscode/arrayscript.js
+++ Desktop/js/.vscode/arrayscript.js
@@ -1,9 +1,31 @@
 const todoList=[];
 
+renderTodoList();
+
+function renderTodoList() {
+    let todoListHTML='';
+
+for (let i = 0; i< todoList.length; i++){
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+}
+
+console.log(todoListHTML);
+
+document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+}
+
+
 function add() {
     const nameElement = document.querySelector('.js-name');
      todoList.push(nameElement.value);
      console.log(todoList);
+
+     nameElement.value='';
+
+     renderTodoList();
 
     
 }
