4764e2334bf3e863587a73d011b58a63f049cd00
RojanShrestha77 <shresrozan555@gmail.com>
2025-11-16 14:21:38 +0545

    hi

--- Desktop/JavaScript/NodeJs/app.js
+++ Desktop/JavaScript/NodeJs/app.js
@@ -1,29 +1,7 @@
-// Globals = anywhere in the project u can access them
-// __dirname = path tot he current directory
-// __filename = filename
-//  require = function to use modules(commonJS)
-// module = info abou the current module
-// process = info about env where the program is being executed
+const {readFileSync, writeFileSync } = require('fs') 
 
-// app.get 
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.isten
+const first = readFileSync('./content/first.txt','utf8')
 
+writeFileSync('./content/result-sync.txt',`Here is the result ${first}`)
 
-
-const names = require('./4-name')
-const sayHi = require('./5-utils')
-const data = require('./6-alternative-flavour')
-
-//if the module has function and we invoke it that code will run 
-// without assigning importing or calling/invoke it
-require('./7-mind-grenade')
-
-
-console.log(data.singlePerson.name)
-sayHi(names.bimbika)
-sayHi('Rojan')
+console.log(first)
