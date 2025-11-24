5e6afb5c2646191487e8d480c76be222515256e6
RojanShrestha77 <shresrozan555@gmail.com>
2025-11-24 23:23:44 +0545

    commit

--- Desktop/JavaScript/NodeJs/app.js
+++ Desktop/JavaScript/NodeJs/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const {people} = require('./data')
+const { success } = require('zod')
 
 // makes everything inside the methods-public available to the  browser
 app.use(express.static('./methods-public'))
@@ -46,6 +47,40 @@ app.post('/login', (req, res) => {
 
     res.status(401).send('Please Provide Credentails')
 })
+
+// app.put = its used for updating the data
+app.put('/api/people/:id', (req, res)=> {
+    const {id} = req.params
+    const {name} = req.body
+
+    const person = people.find((person) => person.id === Number(id))
+
+    if(!person) {
+        return res.status(400).json( {success: false, msg: `person with the ${id} was not found`})
+    }
+    const newPeople = people.map((person) => {
+        if(person.id === Number(id)){
+            person.name = name
+
+        }
+        return person
+    })
+    res.status(200).json({ success: true, data: newPeople})
+}) 
+
+
+app.delete('/api/people/:id', (req,res) => {
+    const person = people.find((person) => person.id === Number(req.params.id))
+    if(!person) {
+        return res.status(400).json({ success: false, msg: `no person with id ${req.params.id}` })
+    }
+    // keep all the id that do not match the id provided in the url
+
+    const newPeople = people.filter((person) => person.id !== Number(req.params.id));
+    return res.status(200).json({ success: true, data: newPeople})
+})
+
+
 app.listen(5000, () => {
     console.log("Server is running in the port 5000")
 })
